8aa53d9df0f4802f3d72964baedfb97a20e42468
Girish Bhagali <bhagaligirishrevnath@gmail.com>
2025-06-05 09:19:27 +0530

    first commit

---
+++ hello.js
@@ -0,0 +1,75 @@
+/*let fil = 78;
+if(fil %2 == 0){
+    console.log("even")
+}else {
+    console.log("odd")
+}*/
+/*for ( let i=0 ; i<101 ; i++){
+    console.log(i);
+}*/
+
+/* switch case statement_________________________
+let day = 3;
+switch(day){
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+         console.log("Tuesday");
+         break;
+    case 3:
+        console.log("wednesday");
+        break;
+    default:    
+        console.log("weekend");
+}*/
+
+/*let age=17;
+if(age %2==0){
+    console.log("even");
+}else{
+    console.log("odd");
+}*/
+
+/*let age =20;
+if(age >= 18){
+    console.log("are you an adult");
+}else{
+    console.log("Are you teenager");
+}*/
+
+/*if..else..if statement_____________________*/
+let score =55;
+if(score >= 90){
+    console.log("grade:A");
+}else if(score >= 80){
+    console.log("Grade:B");
+}else if(score >= 70){
+    console.log("Grade:C");
+}else if(score >=60){
+    console.log("Grade:D");
+}else{
+    console.log("Fail");
+}
+
+/*for(let i=0; i<10;i++){
+    console.log(i)
+}*/
+/*
+let number = 7;
+switch(number %2){
+    case 1:
+        console.log(number +"even");
+        break;
+    case 2:
+        console.log(number + "odd");
+        break;
+    default:
+        console.log("invalid input");
+}*/
+
+/*for(let number = 1; number <= 20;number++){
+    if(number %2 === 0){
+        console.log(number);
+}
+}*/
---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("hello world")
+let num1 = 29
+num1 ="hello";
+num1=true
+console.log(num1)\
